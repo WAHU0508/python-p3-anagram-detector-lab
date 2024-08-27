a924a3840f3a273d729dfafa97c78fe7ec848213
WAHU0508 <sharongikenye@gmail.com>
2024-08-27 15:15:22 +0300

    Anagrams class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, anagrams):
+        word_list = sorted([letter for letter in self.word])
+        self.anagrams = []
+        for anagram in anagrams:
+            anagram_list = sorted([leter for leter in anagram])
+            if word_list == anagram_list:
+                self.anagrams.append(anagram)
+        return self.anagrams
+
+anagram = Anagram("listen")
+print(anagram.match(['enlists', 'google', 'inlets', 'banana']))
